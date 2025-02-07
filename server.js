0646f713e684418b5d49fbd483bcf4ede62b7167
Hrisikesh <134964791+hrisikesh25@users.noreply.github.com>
2025-02-07 11:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch(err => console.log(err));
